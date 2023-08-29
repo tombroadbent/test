80b55d26a0c17c6f8f70f36592295fb59ca673db
Christian <lan21011@byui.edu>
2023-08-29 16:03:48 -0600

    remove spaces in problem_program.py

--- mission_planner/problem_program.py
+++ mission_planner/problem_program.py
@@ -6,31 +6,28 @@ if gender != 'm' and gender != 'f':
 
 if gender == 'm':
     duration = 104 #weeks
-elif gender == 'f': 
+elif gender == 'f':
     duration = 72 #weeks
 
 start_dateStr = str(input('What is your start date? (YYYY-MM-DD) '))
 start_date = datetime.strptime(start_dateStr, '%Y-%m-%d')
 completion_date = start_date + timedelta(weeks=duration)
 print(completion_date.strftime('%Y-%m-%d'))
+
+
 # transfer_date = str(input(“What is the next transfer date? (YYYY-MM-DD) “))
 # transferBeforeCompletion = transfer_date
 # transferAfterCompletion = transfer_date + 6 weeks
 # If t >= completion_date:
 # 	print("Must be less than completion date")
 
-# transferBeforeCompletion = transfer_date 
+# transferBeforeCompletion = transfer_date
 # transferAfterCompletion = transferBeforeCompletion + 6w
 # while transferAfterCompletion < completion_date:
-# 	transferBeforeCompletion = transferAfterCompletion 
+# 	transferBeforeCompletion = transferAfterCompletion
 # 	transferAfterCompletion = transferBeforeCompletion + 6w
 
 # print("Closest transfer date before completion:", transferBeforeCompletion)
 # print("Closest transfer date after completion:", transferAfterCompletion)
-
-
-
-# Idea:
-# Parameter about transfer
 
 
