ae43027b2c676795f0fa7a47c85ef746ffd85763
Christian <lan21011@byui.edu>
2023-08-30 12:12:32 -0600

    implement basic functionality

--- mission_planner/problem_program.py
+++ mission_planner/problem_program.py
@@ -13,21 +13,21 @@ start_dateStr = str(input('What is your start date? (YYYY-MM-DD) '))
 start_date = datetime.strptime(start_dateStr, '%Y-%m-%d')
 completion_date = start_date + timedelta(weeks=duration)
 print(completion_date.strftime('%Y-%m-%d'))
+print()
 
+transfer_dateStr = str(input('What is the next transfer date? (YYYY-MM-DD) '))
+transfer_date = datetime.strptime(transfer_dateStr, '%Y-%m-%d')
+if transfer_date >= completion_date:
+    print("Must be less than completion date")
 
-# transfer_date = str(input(“What is the next transfer date? (YYYY-MM-DD) “))
-# transferBeforeCompletion = transfer_date
-# transferAfterCompletion = transfer_date + 6 weeks
-# If t >= completion_date:
-# 	print("Must be less than completion date")
+transferBeforeCompletion = transfer_date
+transferAfterCompletion = transfer_date + timedelta(weeks=6)
 
-# transferBeforeCompletion = transfer_date
-# transferAfterCompletion = transferBeforeCompletion + 6w
-# while transferAfterCompletion < completion_date:
-# 	transferBeforeCompletion = transferAfterCompletion
-# 	transferAfterCompletion = transferBeforeCompletion + 6w
+while transferAfterCompletion < completion_date:
+    transferBeforeCompletion = transferAfterCompletion
+    transferAfterCompletion = transferBeforeCompletion + timedelta(weeks=6)
 
-# print("Closest transfer date before completion:", transferBeforeCompletion)
-# print("Closest transfer date after completion:", transferAfterCompletion)
+print("Closest transfer date before completion:", transferBeforeCompletion.strftime('%Y-%m-%d'))
+print("Closest transfer date after completion:", transferAfterCompletion.strftime('%Y-%m-%d'))
 
 
